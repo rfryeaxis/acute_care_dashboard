8cb9d3fe8407a25c7ae0716baea0f6aa76441e42
rfryeaxis <rfrye@axisgroup.com>
2015-07-01 15:38:01 -0400

    Build 1
    started using bootstrap template

---
+++ acute_care_dashboard.js
@@ -0,0 +1,30 @@
+/*global require, alert*/
+/*
+ * 
+ * @owner Enter you name here (xxx)
+ */
+/*
+ *    Fill in host and port for Qlik engine
+ */
+var config = {
+	host: window.location.hostname,
+	prefix: "/",
+	port: window.location.port,
+	isSecure: window.location.protocol === "https:"
+};
+require.config( {
+	baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port: "") + config.prefix + "resources"
+} );
+
+require( ["js/qlik"], function ( qlik ) {
+	qlik.setOnError( function ( error ) {
+		alert( error.message );
+	} );
+
+	//callbacks -- inserted here --
+	//open apps -- inserted here --
+	
+	//get objects -- inserted here --
+	//create cubes and lists -- inserted here --
+
+} );
